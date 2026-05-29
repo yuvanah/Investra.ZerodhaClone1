71c50d2c8d87391dfb32977849adb2c620fd968e
Mithun Yuvanah <yuvanah@gmail.com>
2026-05-29 10:28:29 +0530

    added dashboard component structures

---
+++ dashboard/src/components/Dashboard.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+
+import Apps from "../App";
+import Funds from "./Funds";
+import Holdings from "./Holdings";
+import Orders from "./Orders";
+import Position from "./Position";
+import Summary from "./Summary";
+import Watchlist from "./Watchlist";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <Watchlist />
+
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Summary />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/positions" element={<Position />} />
+          <Route path="/funds" element={<Funds />} />
+          <Route path="/apps" element={<Apps />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ dashboard/src/components/Position.jsx
@@ -0,0 +1,27 @@
+const Positions = () => {
+  return (
+    <>
+      <h3 className="title">Positions (2)</h3>
+
+      <div className="order-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Instrument</th>
+              <th>Qty.</th>
+              <th>Avg.</th>
+              <th>LTP</th>
+              <th>P&amp;L</th>
+              <th>Chg.</th>
+            </tr>
+          </thead>
+
+          <tbody></tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Positions;
