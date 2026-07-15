b8f51248f8d3f84689c0132478668f119427df44
Mithun Yuvanah <yuvanah@gmail.com>
2026-07-15 21:20:07 +0530

    app.jsx spell mistak of notfound

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
 
-import NotFound from "./landing_page/Notfound";
+import NotFound from "./landing_page/NotFound";
 import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
 
